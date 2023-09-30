b266fb85e1d5cb4351a4b986ad66211ca705db3f
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-09-30 21:17:12 +0300

    Update bots.py

--- bots.py
+++ bots.py
@@ -1,24 +1,13 @@
-from telethon import TelegramClient
-from telethon import TelegramClient, events, Button, errors
-from telethon.tl.functions.channels import JoinChannelRequest
+from telethon import Button, errors
 from telethon.sync import TelegramClient, events
-from telethon import functions, types
-from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.sessions import StringSession
-import asyncio, json, os, re
-from time import sleep
+import os
 api_id = 2192036
 api_hash = '3b86a67fc4e14bd9dcfc2f593e75c841'
-bot_token = '5823930916:AAEeUrJU3TEV4RNi4kKlxw6F-KvHQmhkwCc'
-
-# We have to manually call "start" if we want an explicit bot token
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
-
-# But then we can use the client instance as usual
-async def Add_NUMBER(event, api_id, api_hash, phone_numbee):
+bot_token = '6341728809:AAHvtrTTIZmuX-N11HM5830wImqqDaiHd5g'
+bot = TelegramClient('bok', api_id, api_hash).start(bot_token=bot_token)
+async def Add_NUMBER(event ,phone_number):
     try:
-        phone_number = phone_numbee.replace('+', '').replace(' ', '')
-        iqthon = TelegramClient(phone_numbee, api_id, api_hash)
+        iqthon = TelegramClient(phone_number, 2192036, '3b86a67fc4e14bd9dcfc2f593e75c841')
         await iqthon.connect()
 
         if not await iqthon.is_user_authorized():
@@ -43,54 +32,35 @@ async def Add_NUMBER(event, api_id, api_hash, phone_numbee):
     except Exception as error:
         return str(error)
 
-@bot.on(events.CallbackQuery(data="start"))
-async def Callbacllks(event):
-    client = open('prift.txt', 'r').read().replace('\n', '')
-    c = os.popen(f"screen -S {client} -dm bash -c 'python3 Dex_nshr.py; exec sh'")
-    print(c)
-    if c:
-        try:
-            await event.edit(c.read())
-        except:
-            await event.edit('True')
-    else:
-        try:
-            await event.edit(c.errors)
-        except:
-            await event.edit("False")
 @bot.on(events.CallbackQuery(data="add_number"))
 async def Callbacks(event):
     await event.delete()
     try:
         # get information from user
         async with bot.conversation(event.chat_id, timeout=300) as conv:
-            await conv.send_message('Api i ?')
-            api_id_msg = await conv.get_response()
-            api_id = api_id_msg.text
-
-            await conv.send_message('Api hash ?')
-            api_hash_msg = await conv.get_response()
-            api_hash_msg = api_hash_msg.text
-
             await conv.send_message('Phone number ?')
             phone_number_msg = await conv.get_response()
             phone_number_msg = phone_number_msg.text
-            open('prift.txt' , 'w').write(phone_number_msg)
-            await conv.send_message(f'''
-Api i : `{api_id}`
-Api hash : `{api_hash_msg}`
-Phone number : `{phone_number_msg}`
-
-جاري تسجيل الدخول
-''')
-
-        result = await Add_NUMBER(event, int(api_id), api_hash_msg, phone_number_msg)
+            phone_number = phone_number_msg.replace('+', '').replace(' ', '')
+            open('prift.txt', 'w').write(phone_number)
+            await conv.send_message(f'''ثواني''')
+        result = await Add_NUMBER(event,phone_number)
         await event.reply(result)
-    except Exception as error:
-        pass
+        c = os.popen(f"screen -S {phone_number} -dm bash -c 'python3 new_nshr_DEX.py; exec sh'")
+        if c:
+            try:
+                await event.edit(c.read())
+            except:
+                await event.edit('True')
+        else:
+            try:
+                await event.edit(c.errors)
+            except:
+                await event.edit("False")
+    except :pass
 async def StartButtons(event, role):
     if role == 1:
-        buttons = [[Button.inline("➕", "add_number")],[Button.inline("start", "start")]]
+        buttons = [[Button.inline("➕", "add_number")]]
     await event.reply("›:ُِ 𝗗َِ𝗘َِ𝗫.#¹ :)", buttons=buttons)
 
 
