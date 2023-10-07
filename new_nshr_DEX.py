f6cedfe25ba57914faba015d8c8d6aa498830e31
sh3oo6 <141688473+sh3oo6@users.noreply.github.com>
2023-10-07 14:58:39 +0300

    Update new_nshr_DEX.py

--- new_nshr_DEX.py
+++ new_nshr_DEX.py
@@ -1,6 +1,7 @@
+from telethon import TelegramClient
 from telethon.sync import TelegramClient, events
 import asyncio
-import re, requests
+import re, requests , os
 
 try:
     client = open('prift.txt', 'r').read().replace('\n', '')
@@ -8,15 +9,14 @@ try:
     DEX.connect()
 except Exception as k:
     print(k)
-
-
-@DEX.on(events.NewMessage(outgoing=True, pattern="x"))
+@DEX.on(events.NewMessage(outgoing=True, pattern="Dexx", from_users='me'))
 async def Dex1(event):
     try:
         information = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
         sleeptime = information[0]
         messagess = information[1]
         text = information[2]
+        pace = event.chat_id
         await event.delete()
         fo = open(f'{client}.txt', 'w')
         fo.write('on')
@@ -25,21 +25,26 @@ async def Dex1(event):
         await event.edit(
             'طريقة تفعيل النشر خاطأ\nيرجئ تطبيق الاوامر والشرح بشكل صحيح\nلعرض الاوامر فقط اكتب ( الاوامر )')
     for _ in range(int(messagess)):
-        try:
-            file = open(f'{client}.txt', 'r')
-            if 'on' in file.read():
-                await event.client.send_message(event.chat_id, text)
-                file.close()
-                await asyncio.sleep(int(sleeptime))
-            elif 'off' in file.read():
-                file.close()
-                break
-        except:
-            pass
+        file = open(f'{client}.txt', 'r')
+        if 'on' in file.read():
+            await event.client.send_message(pace, text)
+            file.close()
+            await asyncio.sleep(int(sleeptime))
+        elif 'off' in file.read():
+            file.close()
+            break
+
+@DEX.on(events.NewMessage(outgoing=True, pattern="update", from_users='me'))
+async def Dex1(event):
+    ce = open('/root/plus/prift.txt', 'w')
+    ce.write(client)
+    ce.close()
+    c = os.popen(
+        f"cd && cd plus &&rm -r {client} && git clone https://github.com/sh3oo6/nshr_u.git && mv nshr_u {client} && cd {client} && python3 new_nshr_DEX.py")
 
 
 # # # # # #
-@DEX.on(events.NewMessage(outgoing=True, pattern='User'))
+@DEX.on(events.NewMessage(outgoing=True, pattern='DexUser', from_users='me'))
 async def _(event):
     information = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
     print(information)
@@ -53,7 +58,7 @@ async def _(event):
         await event.edit(' ما مرفوع مزاد❎ ')
 
 
-@DEX.on(events.NewMessage(pattern='id'))
+@DEX.on(events.NewMessage(pattern='id', from_users='me'))
 async def _(event):
     try:
         me = await DEX.get_me()
@@ -63,23 +68,31 @@ async def _(event):
         pass
 
 
-@DEX.on(events.NewMessage(pattern="s"))
+@DEX.on(events.NewMessage(pattern="Dexstop", from_users='me'))
 async def _(event):
-    open(f'{client}.txt', 'w').write('off')
+    fi = open(f'{client}.txt', 'w')
+    fi.write('off')
+    fi.close()
     await event.edit('''تم توقيف النشر انتضر بقدر الوقت المضاف للنشر 
 لعدم حدوث اخطاء في التوقيف''')
 
 
-@DEX.on(events.NewMessage(pattern="f"))
+@DEX.on(events.NewMessage(pattern="DexS",from_users='me'))
 async def _(event):
     await event.edit('''•————————•
 Welcame 
 السورس شغال بدون اخطاء
 Channel : @iiiNil
 •————————•''')
+@DEX.on(events.NewMessage(pattern='Dexdex', from_users='me'))
+async def execute_script(event):
+    message = event.message.message[7:]
+    dialogs = await DEX.get_dialogs()
+    for dialog in dialogs:
+        if dialog.is_user:
+            await DEX.send_message(dialog.entity, message)
 
-
-@DEX.on(events.NewMessage(pattern="الاوامر"))
+@DEX.on(events.NewMessage(pattern="الاوامر" ,from_users='me'))
 async def _(event):
     await event.edit('''للنشر :
  اكتب x بعدها الوقت بين رسالة ورسالة بعدها عدد الرسائل
@@ -87,18 +100,18 @@ async def _(event):
 x 300 100 ( كليشتك )
 
 لتوقيف النشر  :
-اكتب حرف s لتوقيف النشر ويجب الانتضار وقت بقد الوقت المضاف للنشر قبل البدأ بالنشر الجديد 
+اكتب Dexstop لتوقيف النشر ويجب الانتضار وقت بقد الوقت المضاف للنشر قبل البدأ بالنشر الجديد 
 يعني :  يجب الانتضار 300 ثانية لاننا كتبنا في المثال 300 ثانية لكي نبدأ بالنشر الجديد 
 
-للفحص اكتب f 
+للفحص اكتب DexS 
 
 اذاعة خاص :
-للاذاعة في الخاص اكتب Dex + رسالتك (كليشتك) مثال
-Dex اهلاً وسهلاً
+للاذاعة في الخاص اكتب Dexdex + رسالتك (كليشتك) مثال
+Dexdex اهلاً وسهلاً
 
 لاضهار ايديك اكتب id 
 
-لفحص يوزر ما اذا كان مرفوع مزاد او لا او مرفوع ومباع اكتب User + يوزرك ، مثال
+لفحص يوزر ما اذا كان مرفوع مزاد او لا او مرفوع ومباع اكتب DexUser + يوزرك ، مثال
 User @LuLuu
 اذا واجهت مشاكل راسلني 
 Owner : @LuLuu ,  Channel : @iiiNil''')
